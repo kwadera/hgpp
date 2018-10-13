8e9faff2d6c148f8e8450d7909ff39246f7727f4
kwadera <kevin.wadera@gmail.com>
2018-10-14 01:21:32 +0530

    [Kevin]: Basic Layout of Rooms component

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -12,4 +12,47 @@ export class RoomsComponent implements OnInit {
   ngOnInit() {
   }
 
+  rooms = [ 
+    {
+        name: "Class A Room",
+        description: "Tucked away amidst tropical foliage, and located on the ground floor of a 2 unit casita, these recently refreshed Garden View rooms are close to our lobby, restaurants and pools and are a short stroll to our palm-lined beach. Enjoy free WI-Fi while you relax and unwind in a Mayan inspired hammock.",
+        images: [
+          {
+            imagePath: '../assets/images/room_1.jpg',
+            imageAlt: 'Rooms',
+            active: "active"
+          },
+          {
+            imagePath: '../assets/images/restaurant.jpg',
+            imageAlt: 'Restutant'
+          },
+          {
+            imagePath: '../assets/images/lobby.jpg',
+            imageAlt: 'Lobby'
+          }    
+        ],
+
+    },
+    {
+      name: "Class A Room",
+      description: "Tucked away amidst tropical foliage, and located on the ground floor of a 2 unit casita, these recently refreshed Garden View rooms are close to our lobby, restaurants and pools and are a short stroll to our palm-lined beach. Enjoy free WI-Fi while you relax and unwind in a Mayan inspired hammock.",
+      images: [
+        {
+          imagePath: '../assets/images/room_1.jpg',
+          imageAlt: 'Rooms',
+          active: "active"
+        },
+        {
+          imagePath: '../assets/images/restaurant.jpg',
+          imageAlt: 'Restutant'
+        },
+        {
+          imagePath: '../assets/images/lobby.jpg',
+          imageAlt: 'Lobby'
+        }    
+      ],
+
+  }
+]
+
 }
