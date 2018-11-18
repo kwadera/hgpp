e55ab9cd2c2ee396d4898ea0c9e4e9a18091efe6
kwadera <kevin.wadera@gmail.com>
2018-11-18 14:22:38 +0530

    [Kevin]: Making Cheveron for each room toggle independently of other, Also, maintain the state on crossing breakpoints

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -6,15 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rooms.component.css']
 })
 export class RoomsComponent implements OnInit {
+  isCheveronRight = true;
+  isExpandMore = true;
+  chevron_left_src  = '../assets/images/chevron_left.svg';
+  chevron_right_src = '../assets/images/chevron_right.svg';
+  expand_less_src   = '../assets/images/expand_less.svg';
+  expand_more_src   = '../assets/images/expand_more.svg';
+
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleCheveron(image:any, item) {
+    item.toggleCheveronState = !item.toggleCheveronState
+    // alert("Item toggle state for " + item.name + "  is "+ item.toggleCheveronState);
+    if (item.toggleCheveronState) {
+      image.src="../assets/images/chevron_left.svg"
+    } else {
+      image.src="../assets/images/chevron_right.svg"
+    }
+  }
+
+  toggleExpand(image:any, item) {
+    item.toggleCheveronState = !item.toggleCheveronState
+    // alert("Item toggle state for " + item.name + "  is "+ item.toggleCheveronState);
+    if (item.toggleCheveronState) {
+      image.src="../assets/images/expand_less.svg"
+    } else {
+      image.src="../assets/images/expand_more.svg"
+    }
+  }
+
   rooms = [ 
     {
         name: "Class A Room",
+        toggleCheveronState: false,
         description: "Tucked away amidst tropical foliage, and located on the ground floor of a 2 unit casita, these recently refreshed Garden View rooms are close to our lobby, restaurants and pools and are a short stroll to our palm-lined beach. Enjoy free WI-Fi while you relax and unwind in a Mayan inspired hammock.",
         displayImagePath: '../assets/images/restaurant.jpg',
         images: [
@@ -36,6 +64,7 @@ export class RoomsComponent implements OnInit {
     },
     {
       name: "Class B Room",
+      toggleCheveronState: false,
       description: "Tucked away amidst tropical foliage, and located on the ground floor of a 2 unit casita, these recently refreshed Garden View rooms are close to our lobby, restaurants and pools and are a short stroll to our palm-lined beach. Enjoy free WI-Fi while you relax and unwind in a Mayan inspired hammock.",
       displayImagePath: '../assets/images/room_1.jpg',
       images: [
